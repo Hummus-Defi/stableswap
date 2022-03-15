d382c0aa0dc9c2e06eb00b2a7deea34d5d4b3929
OdysseusTheKing <odysseus.the.king@protonmail.com>
2022-03-15 15:43:38 -0700

    Initialize Stableswap Repository

---
+++ contracts/interfaces/IPriceOracleGetter.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.8.9;
+
+/// @title IPriceOracleGetter interface
+interface IPriceOracleGetter {
+    /// @dev returns the asset price in ETH
+    function getAssetPrice(address _asset) external view returns (uint256);
+
+    /// @dev returns the reciprocal of asset price
+    function getAssetPriceReciprocal(address _asset) external view returns (uint256);
+}
---
+++ contracts/test/TestSlippage.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.8.9;
+
+import '../pool/Core.sol';
+
+contract TestSlippage is Core {
+    function testSlippage(
+        uint256 k,
+        uint256 n,
+        uint256 c1,
+        uint256 xThreshold,
+        uint256 x
+    ) external pure returns (uint256) {
+        return _slippageFunc(k, n, c1, xThreshold, x);
+    }
+}
